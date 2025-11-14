8e03b88df036f02e4b6b26ef6e49a502a38835b7
dannybranton <dannybranton@gmail.com>
2025-11-14 08:59:34 -0600

    sheets

--- civ7draft/src/App.tsx
+++ civ7draft/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 import { Leaders, Picks } from './modules/TwoVTwo';
+//import { default as Sheets } from './Sheets';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/* <Sheets></Sheets> */}
       <div id="the_draft">
         <Leaders />
         <Picks />
